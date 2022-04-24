354984e74c39d27d25bc026e07218984230e126a
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-04-24 12:49:03 +0200

    launch initial updated version

--- client_app.py
+++ client_app.py
@@ -1,106 +1,179 @@
-#!/usr/bin/env python3
-
-import argparse
+#!/usr/local/bin/python3
+import socket, argparse
+from Packet import Header, Packet, Payload
+import queue
+import Btcp
 import time
-from btcp.client_socket import BTCPClientSocket
-
-"""This exposes a constant bytes object called TEST_BYTES_128MIB which, as the
-name suggests, is 128 MiB in size. You can send it, receive it, and check it
-for equality on the receiving end.
-
-Pycharm may complain about an unresolved reference. This is a lie. It simply
-cannot deal with a python source file this large so it cannot resolve the
-reference. Python itself will run it fine, though.
-
-You can also use the file large_input.py as-is for file transfer.
-"""
-from large_input import TEST_BYTES_128MIB
+import Runners
 
 
-def btcp_file_transfer_client():
-    """This method should implement your bTCP file transfer client. We have
-    provided a bare bones implementation: a command line argument parser and
-    a normal sequence of
-    - create the client socket
-    - connect
-    - open the file
-    - loop to send all data in the file
-    - shutdown / disconnect
-    - close
+class bTCP_client(Btcp.Btcp):
 
-    If you start the server_app.py, and then this client_app.py, this will
-    transfer the input file to the server process.
+    def __init__(self, source, destination, window, timeout = 10):
+        super().__init__(source, window, timeout)
+        self.destination = destination
+        self.file = None
+        self.data = []
+        self.filepointer = 0
+        self.timelimit = 0
+        self.lastack = -1
+        self.dupAckQueue = queue.PriorityQueue()
 
-    Our rudimentary bTCP sockets already achieve this *on a perfect network*,
-    because they just chunk the data into bTCP segments. You can check that
-    input and output are the same by using the `cmp` command, e.g. for the
-    default filenames:
-        `cmp large_input.py output.file`
-    If they are the same, no output is given.
-    If they differ it will tell you the location of the first difference.
+    def begin(self):
+        self.start()
+        # Start Handshake
+        while not self.connected:
+            self.connected = self.connect(self.destination)
 
-    But because there is no window size negotiation, no checksums, no sequence
-    numbers, etc yet, the receiving queue might overflow, reordered segments
-    end up as reordered data, flipped bits remain flipped, etc.
+    def set_file(self, file2):
+        self.file = file2
+        self.data = file2.to_packets()
+        self.filepointer = 0
 
-    If you need to change anything, feel free to use helper methods.
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-w", "--window",
-                        help="Define bTCP window size",
-                        type=int, default=100)
-    parser.add_argument("-t", "--timeout",
-                        help="Define bTCP timeout in milliseconds",
-                        type=int, default=100)
-    parser.add_argument("-i", "--input",
-                        help="File to send",
-                        default="large_input.py")
-    args = parser.parse_args()
+    def send_file(self):
+        if self.file is None:
+            return
 
-    # Create a bTCP client socket with the given window size and timeout value
-    s = BTCPClientSocket(args.window, args.timeout)
+        print("Start sending file")
+        self.filepointer = 0
 
-    # Connect. By default this doesn't actually do anything: our rudimentary
-    # implementation relies on you starting the server before the client,
-    # and just dumps the entire file into the network immediately.
-    s.connect()
+        while self.filepointer < self.data.__len__() or not self.retransmissionQueue.empty():
 
-    # Actually open the file, read the file, and send the data.
-    with open(args.input, 'rb') as infile:
-        # I'm reading the file in 1 MiB chunks.
-        chunksize = 1_024_000
+            while not self.dupAckQueue.empty():
+                syn, packet = self.dupAckQueue.get_nowait()
+                self.sendbuffer.put((packet.to_bytes(), self.peer))
+                self.retransmissionQueue.put((syn, packet))
+                print(f"Retry of segment {str(syn)}")
+                if self.process_ack(self.timeout) is False:
+                    self.dupAckQueue.put((syn, packet))
 
-        # Read the first chunk. In python 3.8 and up, we can avoid doing this
-        # before the loop *and* at the end of the loop by using the assignment
-        # expression operator instead:
-        # while data := bytearray(infile.read(chunksize)):.
-        #
-        # For efficiency, I'm using a mutable bytearray. This is much faster
-        # than re-assigning the non-sent slice after every call to send.
-        data = bytearray(infile.read(chunksize))
+            i = 0
+            if not self.retransmissionQueue.empty():
+                packets = []
+                try:
+                    while not self.retransmissionQueue.empty() and i < self.window:
+                        packets.append(self.retransmissionQueue.get_nowait())
+                        i += 1
+                except queue.Empty:
+                    pass
 
-        # Outer loop: while new data was successfully read from file.
-        while data:
-            # Loop until all data read was successfully sent. I've added a
-            # small timeout to allow the sendbuffer to empty out a bit before
-            # trying to send more.
-            while data:
-                sent_bytes = s.send(data)
-                del data[:sent_bytes]
-                time.sleep(0.00005)
-            # In outer loop: Read new data.
-            data = bytearray(infile.read(chunksize))
+                for syn, packet in packets:
+                    self.sendbuffer.put((packet.to_bytes(), self.peer))
+                    self.retransmissionQueue.put((syn, packet))
+                    print(f"Sent segment with sequence number {str(syn)} again")
 
-        # If we exit this loop, all data has been read. We exit the with-block
-        # which automatically closes the input file.
+            while i < self.window and self.filepointer < self.data.__len__():
+                self.send_next_packet()
+                i += 1
 
-    # Disconnect, since we're done reading the file and done sending.
-    # Note that by default this doesn't do *anything*.
-    s.shutdown()
+            # Packets have been added to the buffer once. Now check for Ack packets.
+            self.process_acks(self.timeout)
 
-    # Clean up any state
-    s.close()
+        terminated = self.start_termination(self.destination)
+        while not terminated:
+            terminated = self.start_termination(self.destination)
+
+        self.sender.stop()
+        self.receiver.stop()
+        self.sender.join()
+        self.receiver.join()
+        self.sock.close()
+        return True
+
+    def get_next_packet(self):
+        if self.filepointer >= self.data.__len__():
+            return False
+
+        header = Header(self.id, self.synnumber, self.acknumber, 0b0, self.window, len(self.data[self.filepointer]), 0)
+        packet = Packet(header, Payload(self.data[self.filepointer]))
+        packet.payload.fill()
+        packet.set_checksum()
+
+        self.filepointer += 1
+        return packet
+
+    def send_next_packet(self):
+        packet = self.get_next_packet()
+        if packet is False:
+            return False
+        self.sendbuffer.put((packet.to_bytes(), self.peer))
+        self.retransmissionQueue.put((packet.header.synnumber, packet))
+        # print("Sent segment with a sequence number of " + str(self.synnumber))
+        self.synnumber += 1
+        return packet
+
+    def process_acks(self, timeout):
+        self.timelimit = time.time() + timeout
+        duplicateack = False
+        i = self.retransmissionQueue.qsize()
+        while (not self.retransmissionQueue.empty()) and time.time() <= self.timelimit and i > 0:
+            try:
+                packet, addr = self.receivebuffer.get(True, self.timelimit - time.time())
+                packet = Packet.from_bytes(packet)
+                # print("Received ACK with an ack-number of " + str(packet.header.acknumber))
+                if packet.header.id == self.id and \
+                        packet.check_checksum() and \
+                        packet.header.ack == 1 and \
+                        time.time() <= self.timelimit:
+                    i -= 1
+
+                    if packet.header.acknumber == self.lastack\
+                            and duplicateack is False:
+                        # Duplicate ACK detected
+                        duplicateack = True
+                        syn, lastPacket = self.retransmissionQueue.get_nowait()
+                        self.dupAckQueue.put((syn, lastPacket))
+
+                    # Empty retransmissionQueue as long as packets are ACKed
+                    syn, lastPacket = self.retransmissionQueue.get_nowait()
+                    while packet.header.acknumber > syn:
+                        syn, lastPacket = self.retransmissionQueue.get_nowait()
+                    self.retransmissionQueue.put((syn, lastPacket))
+                    # Update lastack
+                    self.lastack = packet.header.acknumber
+            except queue.Empty:
+                pass
+
+    def process_ack(self, timeout):
+        self.timelimit = time.time() + timeout
+        try:
+            packet, addr = self.receivebuffer.get(True, self.timelimit - time.time())
+            packet = Packet.from_bytes(packet)
+            # print("Received ACK with an ack-number of " + str(packet.header.acknumber))
+            if packet.header.id == self.id and \
+                    packet.check_checksum() and \
+                    packet.header.ack == 1 and \
+                    time.time() <= self.timelimit:
+
+                if packet.header.acknumber >= self.synnumber:
+                    return True
+
+                # Empty retransmissionQueue as long as packets are ACKed
+                syn, lastPacket = self.retransmissionQueue.get_nowait()
+                while packet.header.acknumber > syn:
+                    syn, lastPacket = self.retransmissionQueue.get_nowait()
+                self.retransmissionQueue.put((syn, lastPacket))
+
+                if packet.header.acknumber < self.synnumber:
+                    # Duplicate ACK detected
+                    syn, lastPacket = self.retransmissionQueue.get_nowait()
+                    self.dupAckQueue.put((syn, lastPacket))
+                else:
+                    return True
+
+                # Update lastack
+                self.lastack = packet.header.acknumber
+        except queue.Empty:
+            return False
 
 
 if __name__ == "__main__":
-    btcp_file_transfer_client()
+    # Handle arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-w", "--window", help="Define bTCP window size", type=int, default=100)
+    parser.add_argument("-t", "--timeout", help="Define bTCP timeout in milliseconds", type=int, default=1000)
+    parser.add_argument("-i","--input", help="File to send", default="falcon9.png")
+    args = parser.parse_args()
+
+    clientrunner = Runners.ClientRunner(args.window, args.timeout, args.input)
+    clientrunner.start()
