21cd5f44ee109bc802f5ee8d137b0a26b0850d2e
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-04-28 20:01:46 +0200

    implemented client 2.0

--- client_app.py
+++ client_app.py
@@ -1,179 +1,203 @@
 #!/usr/local/bin/python3
 import socket, argparse
+from struct import *
+import btcp_implementation
 from Packet import Header, Packet, Payload
 import queue
-import Btcp
-import time
 import Runners
+import time
+from btcp.btcp_socket import BTCPSocket, BTCPStates
+from btcp.lossy_layer import LossyLayer
+from btcp.constants import *
 
+class bTCP_server(btcp_implementation.Btcp):
 
-class bTCP_client(Btcp.Btcp):
-
-    def __init__(self, source, destination, window, timeout = 10):
+    def __init__(self, source, window = 100, timeout = 10):
         super().__init__(source, window, timeout)
-        self.destination = destination
-        self.file = None
+        self.reassembleQueue = queue.PriorityQueue()
+        self.reassemblesyns = []
         self.data = []
-        self.filepointer = 0
-        self.timelimit = 0
-        self.lastack = -1
-        self.dupAckQueue = queue.PriorityQueue()
+        self.finished = False
 
     def begin(self):
         self.start()
+
         # Start Handshake
         while not self.connected:
-            self.connected = self.connect(self.destination)
+            self.connected = self.listen()
 
-    def set_file(self, file2):
-        self.file = file2
-        self.data = file2.to_packets()
-        self.filepointer = 0
+    def end(self):
+        self.finished = True
+        
+    def flagfinder(self, flag):
+        if flag == "0x0":
+            return False, False, False
+        elif flag == "0x1":
+            return False, False, True
+        elif flag == "0x2":
+            return False, True, False
+        elif flag == "0x3":
+            return False, True, True
+        elif flag == "0x4":
+            return True, False, False
+        elif flag == "0x5":
+            return True, False, True
+        elif flag == "0x6":
+            return True, True, False
+        elif flag == "0x7":
+            return True, True, True
+        
+    def receive_file(self, file):
+        print("Start receiving file")
+        self.data = []
 
-    def send_file(self):
-        if self.file is None:
-            return
+        while self.connected and not self.finished:
 
-        print("Start sending file")
-        self.filepointer = 0
+            #set socket and generate checksum
+            sock = BTCPSocket(self.window, self.timeout)
+        
+            #get information
+            data, addr = self.receivebuffer.get()
 
-        while self.filepointer < self.data.__len__() or not self.retransmissionQueue.empty():
+            #check if header is long enough
+            if len(data) < 16:
+                raise ValueError("header is not long enough")
+            
+            #get first 10 data bytes
+            headr = data[:10]
+            
+            #get payload
+            payload = data[10:]
+        
+            #buiild temp headr
+            header_temp = sock.unpack_segment_header(headr)
+        
+            #unpack header
+            syn_number = header_temp[0]
+            ack_number = header_temp[1]
+            flag_byte = hex(header_temp[2])
+            window = header_temp[3]
+            length = header_temp[4]
+                
+            #this is the given checksum
+            checksum = header_temp[5]
+            
+            #get bools for flags
+            syn, ack, fin = flagfinder(flag_byte)
+            
+            #we calculate our own checksum from our data to compare
+            checksum_comp = sock.in_cksum(sock.build_segment_header(syn_number, ack_number, syn, ack, fin, window, length, 0)
+                                      + payload)
+            
+            #check checksum integrity
+            if checksum == checksum_comp:
+                
+                #disable connection at FIN
+                if fin == True:
+                    print("FIN rec")
+                    self.connected = False
+                    
+                #if syn = ack
+                elif syn_number == self.acknumber:
+                    self.add_data(header_temp, payload)
+                    self.send_ack()
+                    
+                #no order
+                else:
+                    self.add_data(header_temp, payload)
+                    self.send_ack()
+                    
+        timeout = time.time() + 2*self.timeout
+        while not self.finished and time.time() < timeout:
+            self.finished = self.respond_termination(addr, headr, payload)
+        if not self.finished:
+            print("Terminated because of timeout. No ACK packet received from client.")
 
-            while not self.dupAckQueue.empty():
-                syn, packet = self.dupAckQueue.get_nowait()
-                self.sendbuffer.put((packet.to_bytes(), self.peer))
-                self.retransmissionQueue.put((syn, packet))
-                print(f"Retry of segment {str(syn)}")
-                if self.process_ack(self.timeout) is False:
-                    self.dupAckQueue.put((syn, packet))
-
-            i = 0
-            if not self.retransmissionQueue.empty():
-                packets = []
-                try:
-                    while not self.retransmissionQueue.empty() and i < self.window:
-                        packets.append(self.retransmissionQueue.get_nowait())
-                        i += 1
-                except queue.Empty:
-                    pass
-
-                for syn, packet in packets:
-                    self.sendbuffer.put((packet.to_bytes(), self.peer))
-                    self.retransmissionQueue.put((syn, packet))
-                    print(f"Sent segment with sequence number {str(syn)} again")
-
-            while i < self.window and self.filepointer < self.data.__len__():
-                self.send_next_packet()
-                i += 1
-
-            # Packets have been added to the buffer once. Now check for Ack packets.
-            self.process_acks(self.timeout)
-
-        terminated = self.start_termination(self.destination)
-        while not terminated:
-            terminated = self.start_termination(self.destination)
+        print("All data has been received.")
+        print("Starting writing data to file")
+        file.from_packets(file.path, self.data)
 
         self.sender.stop()
         self.receiver.stop()
         self.sender.join()
         self.receiver.join()
         self.sock.close()
-        return True
 
-    def get_next_packet(self):
-        if self.filepointer >= self.data.__len__():
-            return False
-
-        header = Header(self.id, self.synnumber, self.acknumber, 0b0, self.window, len(self.data[self.filepointer]), 0)
-        packet = Packet(header, Payload(self.data[self.filepointer]))
-        packet.payload.fill()
-        packet.set_checksum()
-
-        self.filepointer += 1
-        return packet
-
-    def send_next_packet(self):
-        packet = self.get_next_packet()
-        if packet is False:
-            return False
-        self.sendbuffer.put((packet.to_bytes(), self.peer))
-        self.retransmissionQueue.put((packet.header.synnumber, packet))
-        # print("Sent segment with a sequence number of " + str(self.synnumber))
-        self.synnumber += 1
-        return packet
-
-    def process_acks(self, timeout):
-        self.timelimit = time.time() + timeout
-        duplicateack = False
-        i = self.retransmissionQueue.qsize()
-        while (not self.retransmissionQueue.empty()) and time.time() <= self.timelimit and i > 0:
-            try:
-                packet, addr = self.receivebuffer.get(True, self.timelimit - time.time())
-                packet = Packet.from_bytes(packet)
-                # print("Received ACK with an ack-number of " + str(packet.header.acknumber))
-                if packet.header.id == self.id and \
-                        packet.check_checksum() and \
-                        packet.header.ack == 1 and \
-                        time.time() <= self.timelimit:
-                    i -= 1
-
-                    if packet.header.acknumber == self.lastack\
-                            and duplicateack is False:
-                        # Duplicate ACK detected
-                        duplicateack = True
-                        syn, lastPacket = self.retransmissionQueue.get_nowait()
-                        self.dupAckQueue.put((syn, lastPacket))
-
-                    # Empty retransmissionQueue as long as packets are ACKed
-                    syn, lastPacket = self.retransmissionQueue.get_nowait()
-                    while packet.header.acknumber > syn:
-                        syn, lastPacket = self.retransmissionQueue.get_nowait()
-                    self.retransmissionQueue.put((syn, lastPacket))
-                    # Update lastack
-                    self.lastack = packet.header.acknumber
-            except queue.Empty:
-                pass
-
-    def process_ack(self, timeout):
-        self.timelimit = time.time() + timeout
+    def add_data(self, header, payload):
+        #unpack header
+        syn_number = header[0]
+        ack_number = header[1]
+        flag_byte = hex(header[2])
+        window = header[3]
+        length = header[4]
+            
+        #this is the given checksum
+        checksum = header[5]
         try:
-            packet, addr = self.receivebuffer.get(True, self.timelimit - time.time())
-            packet = Packet.from_bytes(packet)
-            # print("Received ACK with an ack-number of " + str(packet.header.acknumber))
-            if packet.header.id == self.id and \
-                    packet.check_checksum() and \
-                    packet.header.ack == 1 and \
-                    time.time() <= self.timelimit:
-
-                if packet.header.acknumber >= self.synnumber:
-                    return True
-
-                # Empty retransmissionQueue as long as packets are ACKed
-                syn, lastPacket = self.retransmissionQueue.get_nowait()
-                while packet.header.acknumber > syn:
-                    syn, lastPacket = self.retransmissionQueue.get_nowait()
-                self.retransmissionQueue.put((syn, lastPacket))
-
-                if packet.header.acknumber < self.synnumber:
-                    # Duplicate ACK detected
-                    syn, lastPacket = self.retransmissionQueue.get_nowait()
-                    self.dupAckQueue.put((syn, lastPacket))
+            while True:
+                
+                #if self.ack = segment.syn
+                if syn_number == self.acknumber:
+                    
+                    #add data till length
+                    self.data.append(payload[:length])
+                    
+                    #increment ack
+                    self.acknumber += 1
+                    
+                    #get data from que
+                    syn, header, payload = self.reassembleQueue.get_nowait()
+                    
+                    #remove syn_number from queue
+                    self.reassemblesyns.remove(syn_number)
+                    
+                #if segment.syn > self.ack
+                elif syn_number > self.acknumber:
+                    
+                    #add to queue if not in queue
+                    if syn_number not in self.reassemblesyns:
+                        self.reassembleQueue.put((syn_number, header, payload))
+                        self.reassemblesyns.append(syn_number)
+                    break
                 else:
-                    return True
-
-                # Update lastack
-                self.lastack = packet.header.acknumber
+                    break
         except queue.Empty:
-            return False
+            return
+        except TypeError:
+            print("Error")
+        return
+
+    def send_ack(self):
+        #create default payload
+        payload = bytes(1000)
+        
+        #create socket
+        sock = BTCPSocket(self.window, self.timeout)
+            
+        #create checksum
+        checksum = sock.in_cksum(sock.build_segment_header(self.synnumber, self.acknumber, False, True, False, self.window, 0, 0)
+                                 + payload)
+        
+        #build header including checksum
+        header = sock.build_segment_header(self.synnumber, self.acknumber, False, True, false, self.window, 0, checksum)
+        
+        #send packet
+        self.sendbuffer.put((header + payload, self.peer))
 
 
 if __name__ == "__main__":
-    # Handle arguments
+    #Handle arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-w", "--window", help="Define bTCP window size", type=int, default=100)
-    parser.add_argument("-t", "--timeout", help="Define bTCP timeout in milliseconds", type=int, default=1000)
-    parser.add_argument("-i","--input", help="File to send", default="falcon9.png")
+    parser.add_argument("-w", "--window",
+                        help="Define bTCP window size",
+                        type=int, default=100)
+    parser.add_argument("-t", "--timeout",
+                        help="Define bTCP timeout in milliseconds",
+                        type=int, default=100)
+    parser.add_argument("-o", "--output",
+                        help="Where to store the file",
+                        default="output.file")
     args = parser.parse_args()
 
-    clientrunner = Runners.ClientRunner(args.window, args.timeout, args.input)
-    clientrunner.start()
+    serverrunner = Runners.ServerRunner(args.window, args.timeout, args.output)
+    serverrunner.start()
